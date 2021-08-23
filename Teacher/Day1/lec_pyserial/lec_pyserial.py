a6a10ef2a4842515ab455bdebdcfa1e51afb8711
elias <eliaskiss@hotmail.com>
2021-08-23 16:24:45 +0900

    0823

--- Teacher/Day1/lec_pyserial/lec_pyserial.py
+++ Teacher/Day1/lec_pyserial/lec_pyserial.py
@@ -24,6 +24,8 @@ def openSerial(port, baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PAR
     ser.open()
     # ser = serial.Serial(port, baudrate, ...) # open() 콜할 필요없음.
 
+    return ser
+
 ##############################################################################################
 # 시리얼 포트 쓰기
 ##############################################################################################
@@ -42,10 +44,10 @@ def read(ser, size=1, timeout=None):
     readed = ser.read(size)
     return readed
 
-# EOF 만날때까지 Read
+# EOF 만날때까지 Read, Putty의 경우 EOF는 Ctrl + J
 def readEOF(ser):
     readed = ser.readline()
-    return readed
+    return readed[:-1]
 
 # Exit code인 Ctrl + C 이 들어올때까지 Read
 def readUntilExitCode(ser, code=b'\x03'):
@@ -55,11 +57,39 @@ def readUntilExitCode(ser, code=b'\x03'):
         ic(data)
         readed += data
         if data == code:
-            return readed
+            return readed[:-1]
 
-ic(__name__)
-# if __name__ == '__main__':
-#     pass
 
-ser = openSerial('com3')
+# 이코드가 의미하는것은 이 파일 즉 lec_serial.py 모듈을 실행했을때 들어오는 코드
+# ic(__name__)
+if __name__ == '__main__':
+    # 객체생성
+    ser = openSerial('com2')
 
+    # 영문쓰기
+    string = 'Hello World\r\n'
+    # writePort(ser, string.encode())
+    writePortUnicode(ser, string)
+
+    # 한글쓰기
+    string = '안녕 세상아\r\n'
+    writePortUnicode(ser, string)
+
+    # Read 1 byte: 1byte만 읽고 return
+    ic(read(ser))
+
+    # Read 5 byte: 5byte만 읽고 return
+    ic(read(ser, 5))
+    ic(read(size=5, ser=ser))
+
+    # Read with timeout 5 sec: 5초 대기후에 데이터가 없으면 return
+    ic(read(ser, 1, 5))
+
+    # Read Until EOF
+    ic(readEOF(ser))
+
+    # Read Until EOF with Hangul
+    ic(readEOF(ser).decode())
+
+    # Read until exit code (Ctrl + C)
+    ic(readUntilExitCode(ser))
