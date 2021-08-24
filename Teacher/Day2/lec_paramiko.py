3a116723b53d7970919a871a4bc107615414d196
elias <eliaskiss@hotmail.com>
2021-08-24 14:31:29 +0900

    Day2

--- Teacher/Day2/lec_paramiko.py
+++ Teacher/Day2/lec_paramiko.py
@@ -65,6 +65,8 @@ class MySSH():
         if isReturn:
             return readed
 
+    #################################################### SFTP ##########################################################
+
     #######################################################################################
     # Get File From Host (SFTP)
     # srcFilePath: Server(host), dstFilePath: Local(PC, Client)
@@ -150,6 +152,52 @@ class MySSH():
 
         self.ftp_client.rmdir(srcFilePath)
 
+    ################################################SCP################################################################
+
+    ######################################################################
+    # Get file from host with SCP
+    # srcFilePath: Server(host) dstFilePath: Local(PC, client)
+    def getFromHostWithSCP(self, srcFilePath, dstFilePath):
+        if self.scp_client is None:
+            self.scp_client = SCPClient(self.client.get_transport())
+        self.scp_client.get(srcFilePath, dstFilePath)
+
+    ######################################################################
+    # Put file to host with SCP
+    # srcFilePath: Local(PC, client) dstFilePath: Server(host)
+    def putToHostWithSCP(self, srcFilePath, dstFilePath):
+        if self.scp_client == None:
+            self.scp_client = SCPClient(self.client.get_transport())
+        self.scp_client.put(srcFilePath, dstFilePath)
+
+    ######################################################################
+    # Put folder to host with SCP
+    # srcFilePath: Local(PC, client) dstFilePath: Server(host)
+    def putFolderToHostSCP(self, srcDirPath, dstDirPath):
+        if self.scp_client == None:
+            self.scp_client = SCPClient(self.client.get_transport())
+        self.scp_client.put(srcDirPath, dstDirPath, recursive=True)
+
+    ######################################################################
+    # Get folder to host with SCP
+    # srcFilePath: Local(PC, client) dstFilePath: Server(host)
+    def getFolderFromHostSCP(self, srcDirPath, dstDirPath):
+        if self.scp_client == None:
+            self.scp_client = SCPClient(self.client.get_transport())
+        self.scp_client.get(srcDirPath, dstDirPath, recursive=True)
+
+    ######################################################################
+    # Disconnect
+    def disconnect(self):
+        if self.client != None:
+            self.client.close()
+
+        if self.ftp_client != None:
+            self.ftp_client.close()
+
+        if self.scp_client != None:
+            self.scp_client.close()
+
 if __name__ == '__main__':
     ssh = MySSH()
     # elias.kim@lge.com
@@ -161,66 +209,82 @@ if __name__ == '__main__':
             exit()
 
         # 현재 프로세스 목록파일 만들기
-        # ssh.exeCommand('ps -ef > process_list.txt', False)
+        ssh.exeCommand('ps -ef > process_list.txt', False)
 
         # ls-al 명령어로 파일목록 가져오기
-        # filelist = ssh.exeCommand('ls -al', True)
-        #
-        # for file in filelist:
-        #     # print(file, end='')
-        #     # print(file[:-1])
-        #     ic(file[:-1])
+        filelist = ssh.exeCommand('ls -al', True)
+
+        for file in filelist:
+            # print(file, end='')
+            # print(file[:-1])
+            ic(file[:-1])
 
         # Temp 폴더로 이동 후, 파일목록을 가져오기
         # cd temp
         # ls -al
 
         # 각각 실행 후 터미널이 닫힘
-        # ssh.exeCommand('cd temp')
-        # for file in ssh.exeCommand('ls -al', True):
-        #     ic(file[:-1])
+        ssh.exeCommand('cd temp')
+        for file in ssh.exeCommand('ls -al', True):
+            ic(file[:-1])
 
         # ; - 앞의 명령어가 실패해도 다음 명령어를 실행
         # && - 앞의 명령어가 성공했을때 다음 명령어가 실행
         # & - 앞의 명령어는 백그라운드로 실행하고 동시에 뒤의 명령어를 실행
-        # for file in ssh.exeCommand('cd temp;ls -al;pwd', True):
-        #     ic(file[:-1])
+        for file in ssh.exeCommand('cd temp;ls -al;pwd', True):
+            ic(file[:-1])
 
         # 쉘 스크립트 파일 생성 후 실행
-        # ssh.exeCommand('echo "ls -al" > filelist.sh')
-        # ssh.exeCommand('chmod 777 ./filelist.sh')
-        # for file in ssh.exeCommand('./filelist.sh', True):
-        #     ic(file[:-1])
+        ssh.exeCommand('echo "ls -al" > filelist.sh')
+        ssh.exeCommand('chmod 777 ./filelist.sh')
+        for file in ssh.exeCommand('./filelist.sh', True):
+            ic(file[:-1])
 
         # sudo 실행불가
-        # ssh.exeCommand('sudo mkdir /mytemp;student;')
-        # ssh.sudoCommand('mkdir /mytemp')
+        ssh.exeCommand('sudo mkdir /mytemp;student;')
+        ssh.sudoCommand('mkdir /mytemp')
 
+        ##################################################SFTP##########################################################
         # 서버로 부터 파일 가져오기
-        # ssh.getFromHost('./process_list.txt', 'process_list.txt')
+        ssh.getFromHost('./process_list.txt', 'process_list.txt')
 
         # 서버로 파일 올리기
-        # ssh.putToHost('./process_list.txt', 'process_list_2.txt')
+        ssh.putToHost('./process_list.txt', 'process_list_2.txt')
 
         # 서버에 파일 이름변경하기
-        # ssh.renameHostFile('./process_list.txt', 'process_list_old.txt') # 파일명 변경
-        # ssh.renameHostFile('./temp', './temp_old')                       # 폴더명 변경
+        ssh.renameHostFile('./process_list.txt', 'process_list_old.txt') # 파일명 변경
+        ssh.renameHostFile('./temp', './temp_old')                       # 폴더명 변경
 
         # 서버의 파일 삭제
-        # ssh.deleteHostFile('./process_list_old.txt')
+        ssh.deleteHostFile('./process_list_old.txt')
 
         # 서버의 파일목록 가져오기
-        # file_list = ssh.getFileListFromHost('./temp_old')
-        # for file in file_list:
-        #     ic(file)
+        file_list = ssh.getFileListFromHost('./temp_old')
+        for file in file_list:
+            ic(file)
 
         # 서버의 파일속성값을 같이 가져오기
-        # file_list = ssh.getFileAttrListFromHost('./temp_old')
-        # for file in file_list:
-        #     ic(file)
+        file_list = ssh.getFileAttrListFromHost('./temp_old')
+        for file in file_list:
+            ic(file)
 
         # 서버의 폴더삭제
-        # ssh.deleteHostFolder('./temp_old')
+        ssh.deleteHostFolder('./temp_old')
 
+        ###################################################SCP##########################################################
+        # 서버로 부터 파일 가져오기(SCP)
+        ssh.getFromHostWithSCP('./process_list.txt', 'process_list.txt')
+
+        # 서버로 파일 업로드(SCP)
+        ssh.putToHostWithSCP('./process_list.txt', 'process_list_new.txt')
+
+        # 서버에 폴더 업로드(SCP)
+        ssh.putFolderToHostSCP('temp', 'temp')
+
+        # 서버에 폴더 다운로드(SCP)
+        ssh.getFolderFromHostSCP('temp', 'temp2')
     except Exception as e:
         ic('Exception:', e)
+    finally:
+        ssh.disconnect()
+
