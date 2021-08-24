95956aa8bf4e33ceb8e208d2d516d41d5d4dbdde
elias <eliaskiss@hotmail.com>
2021-08-24 11:59:57 +0900

    Day2

--- Teacher/Day2/lec_paramiko.py
+++ Teacher/Day2/lec_paramiko.py
@@ -4,6 +4,7 @@ from scp import SCPClient
 import os
 import stat
 from icecream import ic
+import time
 
 class MySSH():
     def __init__(self):
@@ -42,9 +43,23 @@ class MySSH():
     # Execute Shell Command
     #######################################################################################
     def exeCommand(self, command, isReturn=False):
-        stdin, stdout, stderr = self.client.exec_command(command)
+        # stdin, stdout, stderr = self.client.exec_command(command)
+        stdin, stdout, _ = self.client.exec_command(command)
 
         if isReturn is True:
+            return stdout.readlines()
+
+    #######################################################################################
+    # Execute SUDO Shell Command
+    #######################################################################################
+    def sudoCommand(self, command, isReturn=False):
+        stdin, stdout, stderr = self.client.exec_command('sudo ' + command, get_pty=True)
+
+        time.sleep(5)
+        stdin.write(self.password + '\n')
+        stdin.flush()
+
+        if isReturn:
             return stdout.readlines()
 
 if __name__ == '__main__':
@@ -69,7 +84,29 @@ if __name__ == '__main__':
         #     ic(file[:-1])
 
         # Temp 폴더로 이동 후, 파일목록을 가져오기
+        # cd temp
+        # ls -al
 
+        # 각각 실행 후 터미널이 닫힘
+        # ssh.exeCommand('cd temp')
+        # for file in ssh.exeCommand('ls -al', True):
+        #     ic(file[:-1])
+
+        # ; - 앞의 명령어가 실패해도 다음 명령어를 실행
+        # && - 앞의 명령어가 성공했을때 다음 명령어가 실행
+        # & - 앞의 명령어는 백그라운드로 실행하고 동시에 뒤의 명령어를 실행
+        # for file in ssh.exeCommand('cd temp;ls -al;pwd', True):
+        #     ic(file[:-1])
+
+        # 쉘 스크립트 파일 생성 후 실행
+        # ssh.exeCommand('echo "ls -al" > filelist.sh')
+        # ssh.exeCommand('chmod 777 ./filelist.sh')
+        # for file in ssh.exeCommand('./filelist.sh', True):
+        #     ic(file[:-1])
+
+        # sudo 실행불가
+        # ssh.exeCommand('sudo mkdir /mytemp;student;')
+        ssh.sudoCommand('mkdir /mytemp')
 
     except Exception as e:
         ic('Exception:', e)
